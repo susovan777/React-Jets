e2c60769942902bb2813109c11ab77aa21f0f5c2
Susovan <susovan.sahoo777@gmail.com>
2024-09-05 21:47:07 +0530

    Modified styling

--- 03-travel-list/src/App.js
+++ 03-travel-list/src/App.js
@@ -1,27 +1,71 @@
+const itemData = [
+  {
+    id: 1,
+    description: "Adapter",
+    quantity: 2,
+    packed: true,
+  },
+  {
+    id: 2,
+    description: "Mat",
+    quantity: 1,
+    packed: false,
+  },
+];
 
 function App() {
-  return <div className="App">
-    <Logo />
-    <Form />
-    <PackingList />
-    <Stats />
-  </div>;
+  return (
+    <div className="App">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
 const Logo = () => {
-  return <h1>🏝️ Far Away 👜</h1>;
+  return <h1>🏝️ Far Away ✈️</h1>;
 };
 
 const Form = () => {
   return (
-    <div className="add-form">
-      <h3>What do you need fro your trip? </h3>
-    </div>
+    <form className="add-form">
+      <h3>What do you need for your trip? </h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>ADD</button>
+    </form>
   );
 };
 
 const PackingList = () => {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {itemData.map((item) => {
+          return <Item key={item.id} item={item} />;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>{" "}
+      <button>❌</button>
+    </li>
+  );
 };
 
 const Stats = () => {
