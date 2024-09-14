f45bbcb5f5bb6c11f37b407edb6514d4b3877c6d
Susovan <susovan.sahoo777@gmail.com>
2024-09-14 20:31:49 +0530

    Deployed the project to Vercel

--- 03-travel-list/src/Components/Form.jsx
+++ 03-travel-list/src/Components/Form.jsx
@@ -20,20 +20,22 @@ const Form = ({ onAddItems }) => {
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip? </h3>
-      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option key={num} value={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        placeholder="Item..."
-        value={entry}
-        onChange={(e) => setEntry(e.target.value)}
-      />
-      <button>ADD</button>
+      <div className="form">
+        <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+            <option key={num} value={num}>
+              {num}
+            </option>
+          ))}
+        </select>
+        <input
+          type="text"
+          placeholder="Item..."
+          value={entry}
+          onChange={(e) => setEntry(e.target.value)}
+        />
+        <button>ADD</button>
+      </div>
     </form>
   );
 };
