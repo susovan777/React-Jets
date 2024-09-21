6379581d92b22c470cdfe0238d95106e633cdf34
Susovan <susovan.sahoo777@gmail.com>
2024-09-21 20:50:14 +0530

    Done the static part

--- 05-usepopcorn/src/App.js
+++ 05-usepopcorn/src/App.js
@@ -1,6 +1,10 @@
-// import { MovieData, WatchedData } from "./TempData";
+import { useState } from "react";
+import { MovieData, WatchedData } from "./TempData";
 
 function App() {
+  const [movies, setMovies] = useState(MovieData);
+  const [watched, setWatched] = useState(WatchedData);
+
   return (
     <div>
       <nav className="nav-bar">
@@ -10,11 +14,80 @@ function App() {
         </div>
 
         <input className="search" type="text" placeholder="Search movies..." />
-        
+
         <p className="num-result">
           Found <strong>3</strong> results
         </p>
       </nav>
+
+      <div className="main">
+        <div className="box">
+          <button className="toggle-btn">-</button>
+          <ul className="list">
+            {movies.map((movie) => {
+              return (
+                <li>
+                  <img src={movie.Poster} alt={movie.Title} />
+                  <h2>{movie.Title}</h2>
+                  <p>
+                    <span>📆</span> {movie.Year}
+                  </p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <div className="box">
+          <button className="toggle-btn">+</button>
+          <div className="summary">
+            <h2>Movies you watched</h2>
+            <div>
+              <p>
+                <span>🎦</span>
+                <span>2 movies</span>
+              </p>
+              <p>
+                <span>⭐️</span>
+                <span>9.3</span>
+              </p>
+              <p>
+                <span>🌟</span>
+                <span>8.65</span>
+              </p>
+              <p>
+                <span>⏳</span>
+                <span>132 min</span>
+              </p>
+            </div>
+          </div>
+
+          <ul className="list">
+            {watched.map((movie) => {
+              return (
+                <li>
+                  <img src={movie.Poster} alt={movie.Title} />
+                  <h2>{movie.Title}</h2>
+                  <div>
+                    <p>
+                      <span>⭐️</span>
+                      <span>{movie.userRating}</span>
+                    </p>
+                    <p>
+                      <span>🌟</span>
+                      <span>{movie.imdbRating}</span>
+                    </p>
+                    <p>
+                      <span>⏳</span>
+                      <span>{movie.runtime} min</span>
+                    </p>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
