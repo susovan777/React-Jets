9ab769f0aa80e28e42cdd0f0abe0ca3e9268cf54
Susovan <susovan.sahoo777@gmail.com>
2024-09-21 18:20:17 +0530

    Styled navbar

--- 05-usepopcorn/src/App.js
+++ 05-usepopcorn/src/App.js
@@ -3,7 +3,18 @@
 function App() {
   return (
     <div>
-      
+      <nav className="nav-bar">
+        <div className="logo">
+          <span>🍿</span>
+          <h1>UsePopcorn</h1>
+        </div>
+
+        <input className="search" type="text" placeholder="Search movies..." />
+        
+        <p className="num-result">
+          Found <strong>3</strong> results
+        </p>
+      </nav>
     </div>
   );
 }
