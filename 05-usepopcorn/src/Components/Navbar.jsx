560c249206af5cca42956288b028b54426399049
Susovan <susovan.sahoo777@gmail.com>
2024-09-23 20:05:15 +0530

    Modified NavBar component: Added children prop

--- 05-usepopcorn/src/Components/Navbar.jsx
+++ 05-usepopcorn/src/Components/Navbar.jsx
@@ -1,16 +1,12 @@
-const Navbar = () => {
+import Search from "./Nav Components/Search";
+import Logo from "./Nav Components/Logo";
+
+const Navbar = ({ children }) => {
   return (
     <nav className="nav-bar">
-      <div className="logo">
-        <span>🍿</span>
-        <h1>UsePopcorn</h1>
-      </div>
-
-      <input className="search" type="text" placeholder="Search movies..." />
-
-      <p className="num-result">
-        Found <strong>3</strong> results
-      </p>
+      <Logo />
+      <Search />
+      {children}
     </nav>
   );
 };
