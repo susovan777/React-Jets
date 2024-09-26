7ba8f5aa37550c13d66d1a4e566e9781e80d5ffb
Susovan <susovan.sahoo777@gmail.com>
2024-09-26 17:59:59 +0530

    Created a new component StarRating.jsx

---
+++ 05-usepopcorn/src/Components/StarRating.jsx
@@ -0,0 +1,14 @@
+const StarRating = () => {
+  return (
+    <div>
+      <div>
+        {Array.from({ length: 5 }, (_, i) => (
+          <span>S{i + 1}</span>
+        ))}
+      </div>
+      <p>10</p>
+    </div>
+  );
+};
+
+export default StarRating;
