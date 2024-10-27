780d2d95f7bf459c60e8a69d3cd3a02347fbcb51
Susovan <susovan.sahoo777@gmail.com>
2024-10-27 07:05:12 +0530

    Added useReducer function: as overview

--- 07-quiz-app/src/Starter Files/DateCounter.js
+++ 07-quiz-app/src/Starter Files/DateCounter.js
@@ -1,7 +1,16 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  console.log(state, action);
+  if (action.type === "inc") return state + action.payload;
+  if (action.type === "dec") return state - action.payload;
+  if (action.type === "setCount") return action.payload;
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0); // useReducer hook
+
   const [step, setStep] = useState(1);
 
   // This mutates the date object.
@@ -10,16 +19,19 @@ function DateCounter() {
 
   const dec = function () {
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    // setCount((count) => count - step);
+    dispatch({ type: "dec", payload: 1 });
   };
 
   const inc = function () {
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    // setCount((count) => count + step);
+    dispatch({ type: "inc", payload: 1 });
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    // setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: +e.target.value });
   };
 
   const defineStep = function (e) {
@@ -27,7 +39,7 @@ function DateCounter() {
   };
 
   const reset = function () {
-    setCount(0);
+    // setCount(0);
     setStep(1);
   };
 
