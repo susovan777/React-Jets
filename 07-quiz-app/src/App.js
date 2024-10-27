f74d0ea3eb14ad8dab56a41ea99dc51c3acef318
Susovan <susovan.sahoo777@gmail.com>
2024-10-27 12:57:24 +0530

    Added json file and useReducer hook implemented

--- 07-quiz-app/src/App.js
+++ 07-quiz-app/src/App.js
@@ -1,6 +1,43 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Components/Header";
 
+const initialState = {
+  questions: [],
+
+  // options: 'loading', 'error', 'ready', 'active', finished'
+  status: "loading",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "failed",
+      };
+
+    default:
+      throw new Error("Unknown action");
+  }
+};
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((data) => data.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch(() => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="App">
       <Header />
