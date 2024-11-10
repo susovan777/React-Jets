e1cc09c2a66450599c13c7327afffd40403cdc73
Susovan <susovan.sahoo777@gmail.com>
2024-11-11 00:01:01 +0530

    Added different pages and linked to routes

--- 08-worldwise/src/App.jsx
+++ 08-worldwise/src/App.jsx
@@ -1,6 +1,20 @@
-const App = () => {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./Pages/Product";
+import Pricing from "./Pages/Pricing";
+import Homepage from "./Pages/Homepage";
+import PageNotFound from "./Pages/PageNotFound";
 
-  return <div>WorldWise</div>;
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
