49e1b19bd321a8341df1777268033b5c8b30a3a9
Susovan <susovan.sahoo777@gmail.com>
2024-11-11 14:15:34 +0530

    Added homepage contents and stylings

--- 08-worldwise/src/Pages/Homepage.jsx
+++ 08-worldwise/src/Pages/Homepage.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-import Navbar from "../Components/Navbar";
+import styles from "./homepage.module.css";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
-    <div>
-      <Navbar />
-      WorldWise
-    </div>
+    <main className={styles.homepage}>
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          Worldwise keep track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+
+        <Link to="/app" className="cta">Start Tracking Now</Link>
+      </section>
+    </main>
   );
 };
 
