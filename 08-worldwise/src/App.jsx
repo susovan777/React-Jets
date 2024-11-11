2b15dd49f8b177746de3adca5b329ae527a09a9f
Susovan <susovan.sahoo777@gmail.com>
2024-11-11 13:35:21 +0530

    Added required starting files

--- 08-worldwise/src/App.jsx
+++ 08-worldwise/src/App.jsx
@@ -3,12 +3,10 @@ import Product from "./Pages/Product";
 import Pricing from "./Pages/Pricing";
 import Homepage from "./Pages/Homepage";
 import PageNotFound from "./Pages/PageNotFound";
-import Navbar from "./Components/Navbar";
 
 const App = () => {
   return (
     <BrowserRouter>
-      
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
--- 08-worldwise/src/Components/Navbar.jsx
+++ 08-worldwise/src/Components/Navbar.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styles from "./navbar.module.css"
 
 const Navbar = () => {
   return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      <li>
-        <NavLink to="/pricing">Pricing</NavLink>
-      </li>
-      <li>
-        <NavLink to="/product">Product</NavLink>
-      </li>
-    </ul>
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/product">Product</NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
