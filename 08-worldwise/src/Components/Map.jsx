184576e1849499dcb16ab4978970bcd35f34d412
Susovan <susovan.sahoo777@gmail.com>
2024-12-04 13:33:33 +0530

    Added leaflet package to the project and added map

--- 08-worldwise/src/Components/Map.jsx
+++ 08-worldwise/src/Components/Map.jsx
@@ -1,5 +1,8 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import styles from "./Map.module.css";
+
+// 🚩 My location: lat: 12.978385, long: 77.623645 (Halasuru, Bangalore)
 
 const Map = () => {
   const navigate = useNavigate();
@@ -9,11 +12,23 @@ const Map = () => {
   const lng = searchParam.get("lng");
 
   return (
-    <div className={styles.map} onClick={() => navigate("form")}>
-      Map
-      <p>
-        Position: {lat}, {lng}
-      </p>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[12.978385, 77.623645]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 };
