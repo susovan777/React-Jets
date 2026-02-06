b6f102be638624ae6e86b3659735f4313fb5bc6f
Susovan <susovan.sahoo777@gmail.com>
2026-02-06 12:48:24 +0530

    App update

--- 89-event-booking/src/App.jsx
+++ 89-event-booking/src/App.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import Footer from "./components/Footer/Footer";
+import DownloadApp from "./components/Sections/DownloadApp/DownloadApp";
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  return (
+    <div>
+      <CssBaseline />
+      <Outlet />
+      <DownloadApp />
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
